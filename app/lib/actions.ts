265404078e0aa30e9fb8fcd2bd0ead5ab9ae4610
Samir <mksamir013@gmail.com>
2025-07-29 16:53:07 +0530

    learned how to mutate data using forms and React Server Actions.

---
+++ app/lib/actions.ts
@@ -0,0 +1,68 @@
+'use server';
+import { z } from 'zod';
+import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ 
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+
+const CreateInvoice = FormSchema.omit({id: true, date: true});
+export async function createInvoice(formdata: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formdata.get('customerId'),
+        amount: formdata.get('amount'),
+        status: formdata.get('status')
+    });
+    // console.log(rawFormData);
+    // console.log(typeof rawFormData.amount);
+     const amountInCents = amount * 100;
+     const date = new Date().toISOString().split('T')[0];
+
+     await sql `
+     insert into invoices (customer_id, amount, status, date)
+     values (${customerId}, ${amountInCents}, ${status}, ${date})
+     `
+
+     revalidatePath('/dashboard/invoices')
+     redirect('/dashboard/invoices');
+}
